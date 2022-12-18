fd4c556fc7fbcbd832ebd41c21a158a4305827bc
yuu kanehiro <info@lpb-studio.com>
2022-12-18 20:40:42 +0900

    ControllerでメソッドインジェクションされるInteractor(Interface)へApplicationとなる各Interctorクラスをbind。register()でbind対象を切り替えられるので、環境変数などで分岐させればテスト用のregisterでbindも可能になる

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Lib\Context\UserContext;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Mock で実行したい場合はコメントアウト
+        $this->registerForInMemory();
+
+        // Mock で実行したい場合はコメント外す
+//        $this->registerForMock();
     }
 
     /**
@@ -24,5 +29,59 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    private function registerForInMemory(){
+        $this->app->singleton(
+            \packages\Domain\Domain\User\UserRepositoryInterface::class,
+            \packages\InMemoryInfrastructure\User\InMemoryUserRepository::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\Account\GetInfo\AccountGetInfoUseCaseInterface::class,
+            \packages\Domain\Application\Account\AccountGetInfoInteractorInterface::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\Auth\Login\AuthLoginUseCaseInterface::class,
+            \packages\Domain\Application\Auth\AuthLoginInteractor::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\User\GetList\UserGetListUseCaseInterface::class,
+            \packages\Domain\Application\User\UserGetListInteractor::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\User\Create\UserCreateUseCaseInterface::class,
+            \packages\Domain\Application\User\UserCreateInteractor::class
+        );
+
+        $this->app->bind(
+            \packages\Domain\Context\UserContextInterface::class,
+            UserContext::class
+        );
+    }
+
+    private function registerForMock(){
+        $this->app->bind(
+            \packages\UseCase\Account\GetInfo\AccountGetInfoUseCaseInterface::class,
+            \packages\MockInteractor\Account\MockAccountGetInfoInteractorInterface::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\Auth\Login\AuthLoginUseCaseInterface::class,
+            \packages\MockInteractor\Auth\MockAuthLoginInteractor::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\User\GetList\UserGetListUseCaseInterface::class,
+            \packages\MockInteractor\User\MockUserGetInteractor::class
+        );
+
+        $this->app->bind(
+            \packages\UseCase\User\Create\UserCreateUseCaseInterface::class,
+            \packages\MockInteractor\User\MockUserCreateInteractor::class
+        );
     }
 }
